d0251dd6c2c1ddaf73d8d72d88001d6416195dea
jangkyoungmoo <jangkyoungmoo@naver.com>
2026-04-22 18:33:40 +0900

    recycle: Refactor 헤더 및 메인 컴포넌트 코드 최적화

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import RootLayout from "../src/pages/RootLayout.jsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import RootLayout from "../src/layout/RootLayout.jsx";
 import Main from "../src/pages/Main/Main.jsx"
 
 function App() {
@@ -7,7 +7,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<RootLayout />}>
-        <Route path="/" element={<Main />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/add" element={<></>}/> {/* 추후 element에 상품 등록 페이지 들어가야함 */}
         </Route>
       </Routes>
     </BrowserRouter>
